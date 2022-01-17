c5c182894ebc865f734bc2871395121e930f2fa7
Sebastian Bader <sebader@microsoft.com>
2022-01-17 17:21:38 +0100

    fixes

--- src/infra/workload/releaseunit/modules/stamp/datasources.tf
+++ src/infra/workload/releaseunit/modules/stamp/datasources.tf
@@ -31,10 +31,10 @@ data "azurerm_storage_account" "global" {
 }
 
 data "azurerm_resource_group" "buildagent" {
-  name = "${var.prefix}-buildagents-rg"
+  name = "${var.prefix}-buildinfra-rg"
 }
 
 data "azurerm_virtual_network" "buildagent" {
-  name                = "${var.prefix}-buildagents-vnet"
+  name                = "${var.prefix}buildinfra-vnet"
   resource_group_name = data.azurerm_resource_group.buildagent.name
 }
