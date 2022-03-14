b87c37b981232fc1efd42e1e20fed56e6e509a0e
Sebastian Bader <sebader@microsoft.com>
2022-03-14 15:21:05 +0100

    Adding default api version to swagger

--- src/app/AlwaysOn.CatalogService/Auth/ApiKeyAttribute.cs
+++ src/app/AlwaysOn.CatalogService/Auth/ApiKeyAttribute.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
@@ -71,6 +72,27 @@ namespace AlwaysOn.CatalogService.Auth
                         Type = "string"
                     }
                 });
+            }
+
+            var versionParameter = operation.Parameters.SingleOrDefault(p => p.Name == "version");
+            if(versionParameter != null)
+            {
+                versionParameter.Schema.Default = new OpenApiString($"{CatalogServiceHelpers.DefaultApiVersionMajor}.{CatalogServiceHelpers.DefaultApiVersionMinor}");
+            }
+        }
+    }
+
+    /// <summary>
+    /// Swagger OperationFilter to add default version to swagger OpenApi definition
+    /// </summary>
+    public class VersionParameterFilter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            var versionParameter = operation.Parameters.SingleOrDefault(p => p.Name == "version");
+            if (versionParameter != null)
+            {
+                versionParameter.Schema.Default = new OpenApiString($"{CatalogServiceHelpers.DefaultApiVersionMajor}.{CatalogServiceHelpers.DefaultApiVersionMinor}");
             }
         }
     }
