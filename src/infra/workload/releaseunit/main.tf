cbe0b87da97a41376c6e358953c7d9c1fc2c5fa3
Sebastian Bader <sebader@microsoft.com>
2022-03-17 14:35:59 +0100

    purge apim

--- src/infra/workload/releaseunit/main.tf
+++ src/infra/workload/releaseunit/main.tf
@@ -15,6 +15,11 @@ provider "azurerm" {
       purge_soft_delete_on_destroy    = false
       recover_soft_deleted_key_vaults = true
     }
+
+    # Purge APIM on destroy (the default is to only soft-delete it)
+    api_management {
+      purge_soft_delete_on_destroy = true
+    }
   }
 }
 
