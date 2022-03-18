cbef17ff545effdfb7c85fb67b803f05fba394e9
Sebastian Bader <sebader@microsoft.com>
2022-03-18 11:51:10 +0100

    fix output, add fqdn for build agent

--- src/infra/workload/ingress_private_connections/outputs.tf
+++ src/infra/workload/ingress_private_connections/outputs.tf
@@ -1,13 +1,8 @@
-output "private_link_service_resource_ids" {
+output "private_link_service_properties" {
   value = tomap({
     for location, inst in var.private_link_service_targets : location => {
       private_link_service_resource_id = azurerm_private_link_service.aks_ingress[location].id
+      buildagent_pe_ingress_fqdn       = trimsuffix(azurerm_dns_a_record.build_agent_ingress_private_endpoint[location].fqdn, ".") # remove trailing dot from fqdn
     }
   })
-}
-
-output "private_link_service_resource_ids_by_location" {
-  value = { for location in var.private_link_service_targets : location => {
-    location = azurerm_private_link_service.aks_ingress[location].id }
-  }
 }
