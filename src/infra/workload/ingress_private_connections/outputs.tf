4581ccd511ca6ee1c1b0f5547c7cced108929be9
Sebastian Bader <sebader@microsoft.com>
2022-03-18 15:02:58 +0100

    attempt fix

--- src/infra/workload/ingress_private_connections/outputs.tf
+++ src/infra/workload/ingress_private_connections/outputs.tf
@@ -8,7 +8,7 @@
 # }
 
 output "stamp_properties" {
-  value = [for location in var.private_link_service_targets : {
+  value = [for location, v in var.private_link_service_targets : {
     location                         = location
     buildagent_pe_ingress_fqdn       = trimsuffix(azurerm_dns_a_record.build_agent_ingress_private_endpoint[location].fqdn, ".") # remove trailing dot from fqdn
     private_link_service_resource_id = azurerm_private_link_service.aks_ingress[location].id
