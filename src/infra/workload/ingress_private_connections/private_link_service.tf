8a0c904ed8de6136256a61bdb0954ed166544e1b
Sebastian Bader <sebader@microsoft.com>
2022-03-18 10:44:51 +0100

    sub id

--- src/infra/workload/ingress_private_connections/private_link_service.tf
+++ src/infra/workload/ingress_private_connections/private_link_service.tf
@@ -5,8 +5,8 @@ resource "azurerm_private_link_service" "aks_ingress" {
   resource_group_name = each.value.resource_group_name
   location            = each.key
 
-  auto_approval_subscription_ids              = [data.azurerm_subscription.current.id]
-  visibility_subscription_ids                 = [data.azurerm_subscription.current.id]
+  auto_approval_subscription_ids              = [data.azurerm_subscription.current.subscription_id]
+  visibility_subscription_ids                 = [data.azurerm_subscription.current.subscription_id]
   load_balancer_frontend_ip_configuration_ids = [each.value.lb_IpConfiguration_Id]
 
   nat_ip_configuration {
