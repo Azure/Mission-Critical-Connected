933425dd9d248ccecfae83582a2049676791c290
Heyko Oelrichs <heoelri@microsoft.com>
2022-03-31 15:45:50 +0200

    bump

--- src/infra/workload/globalresources/frontdoorv2.tf
+++ src/infra/workload/globalresources/frontdoorv2.tf
@@ -1,36 +1,27 @@
 resource "azurerm_cdn_frontdoor_profile" "main" {
-  name                = local.frontdoor_name
-  resource_group_name = azurerm_resource_group.global.name
-
-  sku_name = "Premium_AzureFrontDoor"
-
+  name                     = local.frontdoor_name
+  resource_group_name      = azurerm_resource_group.global.name
   response_timeout_seconds = 120
 
-  tags = local.default_tags
+  sku_name = "Premium_AzureFrontDoor"
+  tags     = local.default_tags
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "default" {
-  name = local.frontdoor_default_frontend_name
-
-  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
-
+  name    = local.frontdoor_default_frontend_name
   enabled = true
 
-  tags = local.default_tags
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
 }
 
 resource "azurerm_cdn_frontdoor_endpoint" "cname" {
-
   count = azurerm_dns_cname_record.app_subdomain != "" ? 1 : 0
 
-  name = local.frontdoor_custom_frontend_name
-  #host_name = trimsuffix(frontend_endpoint.value.fqdn, ".")
+  name    = local.frontdoor_custom_frontend_name
+  enabled = true
+  tags    = local.default_tags
 
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
-  enabled                  = true
-
-  tags = local.default_tags
-
 }
 
 resource "azurerm_cdn_frontdoor_origin_group" "backendapis" {
@@ -56,6 +47,42 @@ resource "azurerm_cdn_frontdoor_origin_group" "backendapis" {
 
 resource "azurerm_cdn_frontdoor_origin_group" "globalstorage" {
   name = "GlobalStorage"
+
+  session_affinity = false
+
+  health_probe {
+    protocol            = "Http"
+    request_type        = "GET"
+    path                = "/"
+    interval_in_seconds = 240
+  }
+
+  load_balancing {
+    sample_size                        = 6
+    successful_samples_required        = 3
+    additional_latency_in_milliseconds = 0
+  }
+
+  cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
+}
+
+resource "azurerm_cdn_frontdoor_origin_group" "staticstorage" {
+  name = "StaticStorage"
+
+  session_affinity = false
+
+  health_probe {
+    protocol            = "Http"
+    request_type        = "GET"
+    path                = "/"
+    interval_in_seconds = 240
+  }
+
+  load_balancing {
+    sample_size                        = 6
+    successful_samples_required        = 3
+    additional_latency_in_milliseconds = 0
+  }
 
   cdn_frontdoor_profile_id = azurerm_cdn_frontdoor_profile.main.id
 }
@@ -85,13 +112,23 @@ resource "azurerm_cdn_frontdoor_origin" "globalstorage-secondary" {
 }
 
 resource "azurerm_cdn_frontdoor_origin" "backendapi" {
-  for_each = var.backends_BackendApis
+  for_each = toset(keys({for i, r in var.backends_BackendApis:  i => r}))
 
-  name      = each.key
-  host_name = each.value.address
-  weight    = each.value.weight
+  name      = var.backends_BackendApis[each.value]["address"]
+  host_name = var.backends_BackendApis[each.value]["address"]
+  weight    = var.backends_BackendApis[each.value]["weight"]
 
   cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.backendapis.id
+}
+
+resource "azurerm_cdn_frontdoor_origin" "staticstorage" {
+  for_each = toset(keys({for i, r in var.backends_StaticStorage:  i => r}))
+
+  name      = var.backends_StaticStorage[each.value]["address"]
+  host_name = var.backends_StaticStorage[each.value]["address"]
+  weight    = var.backends_StaticStorage[each.value]["weight"]
+
+  cdn_frontdoor_origin_group_id = azurerm_cdn_frontdoor_origin_group.staticstorage.id
 }
 
 resource "azurerm_cdn_frontdoor_custom_domain" "test" {
