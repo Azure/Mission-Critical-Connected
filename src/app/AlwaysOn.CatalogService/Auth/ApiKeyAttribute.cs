9982daf6fc1968eb7e1c089eee799c09eeedc27a
Sebastian <11445087+sebader@users.noreply.github.com>
2023-03-16 21:04:30 +0100

    Component updates 03-2023 (#472)
    * updates
    
    * config
    
    * Bump grafana/grafana in /src/infra/monitoring/grafana (#435)
    
    Bumps grafana/grafana from 9.3.1 to 9.3.6.
    
    ---
    updated-dependencies:
    - dependency-name: grafana/grafana
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump node from 19.2.0 to 19.5.0 in /src/app/AlwaysOn.UI (#436)
    
    Bumps node from 19.2.0 to 19.5.0.
    
    ---
    updated-dependencies:
    - dependency-name: node
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * a million updates
    
    * Bump grafana/grafana in /src/infra/monitoring/grafana (#469)
    
    Bumps grafana/grafana from 9.3.6 to 9.4.3.
    
    ---
    updated-dependencies:
    - dependency-name: grafana/grafana
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * adding location
    
    * tf
    
    * rename
    
    * dns
    
    * no zones
    
    * location
    
    * zone back
    
    * updates
    
    * workload identity
    
    * Bump webpack from 5.75.0 to 5.76.2 in /src/app/AlwaysOn.UI (#473)
    
    Bumps [webpack](https://github.com/webpack/webpack) from 5.75.0 to 5.76.2.
    - [Release notes](https://github.com/webpack/webpack/releases)
    - [Commits](https://github.com/webpack/webpack/compare/v5.75.0...v5.76.2)
    
    ---
    updated-dependencies:
    - dependency-name: webpack
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * updates
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/app/AlwaysOn.CatalogService/Auth/ApiKeyAttribute.cs
+++ src/app/AlwaysOn.CatalogService/Auth/ApiKeyAttribute.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Any;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System;
--- src/app/AlwaysOn.HealthService/HealthJob.cs
+++ src/app/AlwaysOn.HealthService/HealthJob.cs
@@ -1,8 +1,12 @@
 ﻿using AlwaysOn.Shared;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,16 +20,20 @@ namespace AlwaysOn.HealthService
     {
         private readonly TimeSpan _checkInterval;
 
+        private readonly ILogger<HealthJob> _logger;
         private readonly HealthCheckService _healthCheckService;
         private readonly SysConfiguration _sysConfig;
+        private readonly TelemetryClient _telemetryClient;
 
         public static HealthReport LastReport { get; private set; }
         public static DateTime LastExecution { get; private set; }
 
-        public HealthJob(SysConfiguration sysConfig, HealthCheckService healthCheckService)
+        public HealthJob(ILogger<HealthJob> logger, SysConfiguration sysConfig, HealthCheckService healthCheckService, TelemetryClient telemetryClient)
         {
+            _logger = logger;
             _sysConfig = sysConfig;
             _healthCheckService = healthCheckService;
+            _telemetryClient = telemetryClient;
             _checkInterval = TimeSpan.FromSeconds(_sysConfig.HealthServiceCacheDurationSeconds);
         }
 
@@ -33,32 +41,47 @@ namespace AlwaysOn.HealthService
         {
             while (!stoppingToken.IsCancellationRequested)
             {
-                var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_sysConfig.HealthServiceOverallTimeoutSeconds));
-                try
+                var requestTelemetry = new RequestTelemetry { Name = $"HealthJob cycle" };
+
+                using (_telemetryClient.StartOperation(requestTelemetry))
                 {
-                    // Run all health checks
-                    LastReport = await _healthCheckService.CheckHealthAsync(cts.Token);
-                    LastExecution = DateTime.Now;
-                }
-                catch (TaskCanceledException)
-                {
-                    // Ignored
-                }
-                catch (Exception e)
-                {
-                    var exceptionEntry = new HealthReportEntry(HealthStatus.Unhealthy, "Exception on running health checks", TimeSpan.Zero, e, null);
-                    var entries = new Dictionary<string, HealthReportEntry>
+                    _logger.LogDebug("Running all health checks");
+                    var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_sysConfig.HealthServiceOverallTimeoutSeconds));
+                    try
                     {
-                        { "HealthCheckerError", exceptionEntry }
-                    };
+                        // Run all health checks
+                        LastReport = await _healthCheckService.CheckHealthAsync(cts.Token);
+                        LastExecution = DateTime.Now;
+                        _logger.LogDebug("Finished all health checks. LastReport.Result={result}", LastReport.Status);
+                        requestTelemetry.Success = true;
+                        requestTelemetry.ResponseCode = HttpStatusCode.OK.ToString();
+                    }
+                    catch (TaskCanceledException e)
+                    {
+                        // Ignored
+                        _logger.LogError(e, "TaskCanceledException during health check(s) execution");
+                        requestTelemetry.Success = false;
+                        requestTelemetry.ResponseCode = HttpStatusCode.InternalServerError.ToString();
+                    }
+                    catch (Exception e)
+                    {
+                        _logger.LogError(e, "Exception during health check(s) execution {message}", e.Message);
+                        requestTelemetry.Success = false;
+                        requestTelemetry.ResponseCode = HttpStatusCode.InternalServerError.ToString();
 
-                    LastReport = new HealthReport(entries, TimeSpan.Zero);
-                }
-                finally
-                {
-                    cts.Dispose();
-                }
+                        var exceptionEntry = new HealthReportEntry(HealthStatus.Unhealthy, "Exception on running health checks", TimeSpan.Zero, e, null);
+                        var entries = new Dictionary<string, HealthReportEntry>
+                        {
+                            { "HealthCheckerError", exceptionEntry }
+                        };
 
+                        LastReport = new HealthReport(entries, TimeSpan.Zero);
+                    }
+                    finally
+                    {
+                        cts.Dispose();
+                    }
+                }
                 await Task.Delay(_checkInterval, stoppingToken);
             }
         }
